89c6314744372de658c6180364c1f0c8577ead31
Marek Urbaniak <Marek.Urbaniak@gmail.com>
2021-04-28 12:12:48 +0100

    Fix ApiService building and use freely available API

--- app/src/main/java/com/example/weather/api/ApiService.kt
+++ app/src/main/java/com/example/weather/api/ApiService.kt
@@ -6,11 +6,13 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.reactivex.rxjava3.core.Single
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 interface ApiService {
 
-    @GET("2.5/forecast/hourly?q=London&appid=TODO")
+    @GET("forecast?q=London&units=metric&appid=acfa082c0129b59f417a1179ea735ea8")
     fun fetchHourlyWeather(): Single<Response>
 }
 
@@ -20,7 +22,9 @@ class RetrofitModule {
 
     @Provides
     fun apiService(): ApiService = Retrofit.Builder()
-        .baseUrl("https://pro.openweathermap.org/data/2.5")
+        .baseUrl("https://api.openweathermap.org/data/2.5/")
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create())
         .build()
         .create(ApiService::class.java)
 }
